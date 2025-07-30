fe430ba4c745eb3ec294046d25b744c99220041b
Kevin Lopes <kevinlopes2304@gmail.com>
2025-07-29 22:37:30 -0300

    Adiciona constantes de preços de moedas e implementa a conversão de moeda no evento de submit do formulário

--- scripts.js
+++ scripts.js
@@ -1,4 +1,10 @@
-const from = DocumentTimeline.querySelector("form") 
+//Cotação
+
+const USD_PRICE = 5.5;
+const EUR_PRICE = 6.5;
+const GBP_PRICE = 7.5;
+
+const from = DocumentTimeline.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
 
@@ -10,7 +16,19 @@ amount.addEventListener("input", () => {
 
 // Capturando o evento de submit (enviar) do formulario
 form.onsubmit = (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    
-}
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD_PRICE, "$");
+      break;
+    case "EUR":
+      convertCurrency(amount.value, EUR_PRICE, "€");
+      break;
+    case "GBP":
+      convertCurrency(amount.value, GBP_PRICE, "£");
+  }
+};
+
+//Função para converter a moeda
+function convertCurrency(amount, price, symbol) {}

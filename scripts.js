388c6492e16dff0086e2ce27efa1b6c5d5144d5a
Kevin Lopes <kevinlopes2304@gmail.com>
2025-07-29 22:29:50 -0300

    Adiciona captura do evento de submit no formulário e define a constante 'currency'

--- scripts.js
+++ scripts.js
@@ -1,7 +1,16 @@
+const from = DocumentTimeline.querySelector("form") 
 const amount = document.getElementById("amount");
+const currency = document.getElementById("currency");
 
 // Manipulando o input amount para receber somente números
 amount.addEventListener("input", () => {
   const hasCharactersRegex = /\D+/g;
   amount.value = amount.value.replace(hasCharactersRegex, "");
 });
+
+// Capturando o evento de submit (enviar) do formulario
+form.onsubmit = (event) => {
+    event.preventDefault();
+
+    
+}
